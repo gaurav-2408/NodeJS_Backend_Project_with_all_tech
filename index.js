ca2afc93f8f1cb4678ee99ae0b7022ebc74f1985
Gaurav Jain <gauravrameshchandr.j@dish.com>
2024-10-28 12:17:33 +0530

    database mongodb connected

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("first commit")
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+//db connnection
+mongoose.connect("mongodb+srv://gauravjain200024:root@learnmongodb.gmcpz.mongodb.net/learnMongoDb?retryWrites=true&w=majority&appName=LearnMongoDb")
+.then(()=>{
+    console.log(`connected to database`)
+
+    //app connection
+    app.listen(3000, ()=>{
+        console.log(`port 3000`)
+    })
+}).catch(()=>{
+    console.log(`database not connected`)
+})
+
