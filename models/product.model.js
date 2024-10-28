6c31fe5d84387c6229fa84ce62e5937cf3b631c9
Gaurav Jain <gauravrameshchandr.j@dish.com>
2024-10-28 12:18:28 +0530

    models included

---
+++ models/product.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Product name is required']
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    image: {
+        type: String,
+        required: false
+    }
+}, {
+    timeStamps: true
+}
+)
